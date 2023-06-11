fae60943c8d1d4802547201294608126847ab33b
veiksmas <autocadbim3@gmail.com>
2023-06-11 15:07:06 +0100

    ANTRA UZDUOTIS

---
+++ Uzduotis02/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+using System;
+
+namespace Uzduotis02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Mano vardas\nRita");
+        }
+
+
+            
+       
